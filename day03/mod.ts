4249900bc154a348ed14364112f3df7766739e5f
Flxw <lxcalflxw@gmail.com>
2022-12-03 16:22:43 +0100

    feat: day03

--- day03/mod.ts
+++ day03/mod.ts
@@ -1,18 +1,21 @@
-import { readFileLines } from "../utils.ts";
+import { readFileLines } from '../utils.ts';
 
-const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 export function day03_01() {
     const lines = readFileLines('/day03/input.txt');
     let sum = 0;
     for (const line of lines) {
-        const share: string[] = []
-        const firstComp = line.slice(0, line.length / 2)
-        const secondComp = line.slice(line.length / 2).split('')        
+        const share: string[] = [];
+        const firstComp = line.slice(0, line.length / 2);
+        const secondComp = line.slice(line.length / 2).split('');
 
         for (const char of firstComp) {
-            if (secondComp.find(c => c === char) && !(share.find(c => c === char))) {
-                share.push(char)
+            if (
+                secondComp.find((c) => c === char) &&
+                !share.find((c) => c === char)
+            ) {
+                share.push(char);
             }
         }
 
@@ -22,9 +25,26 @@ export function day03_01() {
     }
 
     console.log(sum);
-    
 }
 
 export function day03_02() {
+    const lines = readFileLines('/day03/input.txt');
+    const chunks = [];
+    for (let i = 0; i < lines.length; i += 3) {
+        chunks.push(lines.slice(i, i + 3));
+    }
 
+    let sum = 0;
+    for (const chunk of chunks) {
+        let shared = '';
+        for (const char of chunk[0]) {
+            if (chunk[1].includes(char) && chunk[2].includes(char)) {
+                shared = char;
+            }
+        }
+
+        sum += chars.indexOf(shared) + 1;
+    }
+
+    console.log(sum);
 }
