daeee1aa51737e95e8d5743147d2097d0a769cf5
Flxw <lxcalflxw@gmail.com>
2022-12-04 11:06:37 +0100

    feat: day04 part 1

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 import { day01_01, day01_02 } from './day01/mod.ts';
 import { day02_01, day02_02 } from "./day02/mod.ts";
 import { day03_01, day03_02 } from "./day03/mod.ts";
+import { day04_01,day04_02 } from "./day04/mod.ts";
 
 // Day01
 console.log('Day 01:');
@@ -18,4 +19,10 @@ console.log();
 console.log('Day 03:');
 day03_01();
 day03_02();
+console.log();
+
+// Day04
+console.log('Day 04:');
+day04_01();
+day04_02();
 console.log();
